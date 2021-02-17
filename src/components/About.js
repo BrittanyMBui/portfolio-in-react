11d6318b3771cbf3e6e838cbac7dd75e90da5689
brittanymbui <brittymbui@gmail.com>
2021-02-16 16:03:58 -0800

    Adds short bio in the About section

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ return (
     <div id="about">
         <h1>About</h1>
         <p>
+            I'm a full stack engineer based in San Francisco, CA. My focus is mainly on back-end functionality with some emphasis on front-end user experience functionality. I spend most of my time coding because I genuinely <strong>love</strong> it, but when I'm not coding you might find me cooking, binging Netflix, or playing RPG's.
         </p>
     </div>
     );
