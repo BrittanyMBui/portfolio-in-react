ffdca3269e5241802bcb95708e1b73bb6581744e
brittanymbui <brittymbui@gmail.com>
2021-03-15 11:15:02 -0700

    Adds styling

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ return (
     <div id="about">
         <h1>About</h1>
         <p>
-            I'm a full stack engineer based in San Francisco, CA. 
+            I'm a full stack software engineer based in San Francisco, CA. 
             <br />My focus is on back-end functionality with emphasis on front-end user experience functionality. 
             <br />I spend most of my time coding because I genuinely <strong>love</strong> it, 
             <br />but when I'm not coding you might find me working out, binging Netflix, or playing RPG's.
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -63,7 +63,7 @@ render () {
                     {this.state.showProjectZero ? 'Hide' : 'View' } Project Description
                 </button>
                 <div style={{ display: this.state.showProjectZero ? 'block' : 'none' }}>
-                <p>
+                <p className="project-desc">
                     This was my first coding project at General Assembly.
                     I built an in-browser interactive game where a user
                     could care for a pet until it dies of old age or neglect.
@@ -85,7 +85,7 @@ render () {
                     {this.state.showProjectOne ? 'Hide' : 'View' } Project Description
                 </button>
                 <div style={{ display: this.state.showProjectOne ? 'block' : 'none' }}>
-                <p>
+                <p className="project-desc">
                     This was my second coding project at General Assembly, and also my
                     first group project experience. My partner and I chose to build out a personal
                     journaling app, where a user could sign-up or log-in to a private profile and
@@ -110,7 +110,7 @@ render () {
                     {this.state.showProjectTwo ? 'Hide' : 'View' } Project Description
                 </button>
                 <div style={{ display: this.state.showProjectTwo ? 'block' : 'none' }}>
-                <p>
+                <p className="project-desc">
                     This was my third coding project at General Assembly. The purpose of this app was simply to display
                     knowledge and skills of the Django framework. I built it without a real-world use-case in mind but had a lot
                     of fun with building it out. In this app, a user could sign-up or log-in to a profile page. From their profile page,
@@ -119,7 +119,7 @@ render () {
                     'meals' and choose whether it was breakfast, lunch, or dinner. Each 'feeding' would display in a form on the creature's page.
                     I added conditional logic to display a message to the user if their creature had not been fed, or whether the creature had
                     been fed all meals for the day. I built this project using Django, Materialize CSS, CSS, HTML, and used PostgreSQL as
-                    the database.
+                    the database. Link coming soon! I am happy to demo this app to anyone who may be interested!
                 </p>
                 </div>
             </article>
@@ -129,14 +129,14 @@ render () {
                     {this.state.showProjectThree ? 'Hide' : 'View' } Project Description
                 </button>
                 <div style={{ display: this.state.showProjectThree ? 'block' : 'none' }}>
-                <p>
+                <p className="project-desc">
                     This was a project I chose to build in my free-time after finishing the Django curriculum at General Assembly.
                     I decided to take the skills I learned and apply them to an app that would have a real-world use-case.
                     This is a very simple and user friendly to-do list. In this app, a user could sign-up or log-in to their profile page. 
                     From their profile page, they could view the titles and dates of each to-do task. Clicking on a task would bring the user
                     to a detailed view page of the to-do task. From there, they can edit or delete the task as well.
                     From the profile page, the user can also add a new to-do task. I built this project using Python, Django, CSS, and PostgreSQL
-                    as the database.
+                    as the database. Link coming soon! I am happy to demo this app to anyone who may be interested!
                 </p>
                 </div>
             </article>
@@ -146,7 +146,7 @@ render () {
                     {this.state.showProjectFour ? 'Hide' : 'View' } Project Description
                 </button>
                 <div style={{ display: this.state.showProjectFour ? 'block' : 'none' }}>
-                <p>
+                <p className="project-desc">
                     This was my capstone project at General Assembly. I chose to build a space for women and underrepresented people in technology. I built this
                     project using MongoDB, Express, React, and Node. I did all of the CSS and styling from scratch.
                     This application includes a customized news feed after user login and a journal-like page where
