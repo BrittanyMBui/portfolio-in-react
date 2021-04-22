1c66c90f734ef06b3086d9b46f2725074f8cdf24
brittanymbui <brittymbui@gmail.com>
2021-04-22 14:04:18 -0700

    Tweaks about section

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,10 @@ const Home = () => {
     return (
         <div id="home">
             <div className="home-text">
-                <p>Hello, world! I am</p>
+                <p>Hello, World! I am</p>
                 <h1 className="home-header">Brittany Bui.</h1>
-                <p>Full Stack Software Engineer</p>
-                <p>living in San Francisco, CA.</p>
+                <p>A Full Stack Software Engineer</p>
+                <p>based out of San Francisco, CA.</p>
             </div>
         </div>
     );
