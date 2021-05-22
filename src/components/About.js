78d81efc06c24d167111ef4c855284fbbc039409
brittanymbui <brittymbui@gmail.com>
2021-05-22 09:06:41 -0700

    Adds changes to about section

--- src/components/About.js
+++ src/components/About.js
@@ -5,14 +5,19 @@ return (
         <h1>About</h1>
         <img src="https://imgur.com/EsAUm8Z.jpg" alt="brittany bui profile" className="brittany" />
         <p>
-        I come from quite a diverse background as far as life and work experiences. I spent the better part of the last decade bartending, owning a small business, and intermittently pursuing my dream career of software engineering. It wasn’t until late 2020 after officially losing my bartending job to Covid-19 that I was able to finally put 100% into making my dream a reality. I have a deep passion and genuine enjoyment for developing backend functionality. I especially enjoy working with APIs and building out backend functionality using Express, Node, and MongoDB.
+        I come from quite a diverse background as far as life and work experiences. 
+        I spent the better part of the last decade bartending, owning a small business, 
+        and intermittently pursuing my dream career of software engineering. 
+        It wasn’t until late 2020 after officially losing my service industry job to COVID-19 that I was able to finally put 100% into making my dream a reality.
+        I deeply enjoy working on any part of the stack, with emphasis on backend development. I especially love working with and building out APIs.
             <br />I spend most of my time coding because I genuinely <strong>love</strong> it, 
             <br />but when I'm not coding you might find me working out, binging Netflix, or playing RPGs.
             <br />
             <br /><h1>Skills</h1>
-            <p>Frontend/Backend: JavaScript, Python, React.js, Node.js, Express.js, jQuery, HTML5/CSS3, Django, Mongoose, RESTful APIs, TypeScript
-            <br />Databases: MongoDB, PostgreSQL
-            <br />Other: Git, Github, Terminal, UNIX, Linux, VS Code, Heroku, Postman</p>
+            <br /><strong>Frontend</strong>: JavaScript, React.js, jQuery, HTML5/CSS3, TypeScript, Bootstrap
+            <br /><strong>Backend</strong>: Node.js, Express.js, Python, Django, Mongoose, GraphQL, Apollo
+            <br /><strong>Databases</strong>: MongoDB, PostgreSQL
+            <br /><strong>Other</strong>: RESTful APIs, JSON, Authentication, AWS, Linux, Web Sockets
         </p>
     </div>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
             <div className="home-text">
                 <p>Hello, World! I am</p>
                 <h1 className="home-header">Brittany Bui.</h1>
-                <p>A Full Stack Software Engineer</p>
+                <p>Full Stack Software Engineer</p>
                 <p>based out of San Francisco, CA.</p>
             </div>
         </div>
